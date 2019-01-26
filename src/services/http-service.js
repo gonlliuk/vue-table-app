be6a6e477127347d141df638d761f9f6a640a383
Stanislav Voitov <svoitov@exadel.com>
2019-01-26 17:09:36 +0500

    http service added

---
+++ src/services/http-service.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+export default class HTTPService {
+    constructor(baseUrl = '/', headers = {}) {
+        this.http = axios.create({
+            baseUrl,
+            headers: {
+                common: {
+                    ...headers.common,
+                },
+                post: {
+                    'Content-Type': 'application/json',
+                    ...headers.post,
+                },
+            },
+        });
+    }
+
+    get(url = '', params = {}, headers = {}) {
+        return this.http.get(url, { params, headers }).then(res => res.data);
+    }
+
+    post(url = '', data = {}, params = {}, headers = {}) {
+        return this.http.post(url, data, { params, headers }).then(res => res.data);
+    }
+
+    put(url = '', data = {}, params = {}, headers = {}) {
+        return this.http.put(url, data, { params, headers }).then(res => res.data);
+    }
+
+    patch(url = '', data = {}, params = {}, headers = {}) {
+        return this.http.patch(url, data, { params, headers }).then(res => res.data);
+    }
+
+    delete(url = '', params = {}, headers = {}) {
+        return this.http.delete(url, { params, headers }).then(res => res.data);
+    }
+}
