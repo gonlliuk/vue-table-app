bfebffbd6d3529c6afca0ac866c2f16caf52472f
Stanislav Voitov <svoitov@exadel.com>
2019-01-27 14:47:11 +0500

    moved vue devDeps to deps for deployment

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const path = require('path');
 const http = require('http');
 
 const dist = path.resolve(__dirname, 'dist');
-
 const app = express();
-const server = http.createServer(app);
-const port = process.env.PORT || 5000;
 
 app.use(express.static(dist));
 app.use('/*', function(req, res) {
     res.sendFile(path.join(dist, 'index.html'))
 });
 
+const port = process.env.PORT || 5000;
+const server = http.createServer(app);
 server.listen(port);
